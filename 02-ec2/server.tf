2aadc8628c2981051a894c1620ba578fa4c4b14d
Gangadhar <gangadharkalaga@gmail.com>
2024-02-06 22:50:10 +0530

    terraform

--- 02-ec2/server.tf
+++ 02-ec2/server.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "test" {
-  ami           = "ami-06b72b3b2a773be2b"
+  ami           = "ami-0f3c7d07486cad139"
   instance_type = "t2.micro"
 
   tags = {
