46c30ae14786d10f567a493939b97ddd4afc4958
Gangadhar <gangadharkalaga@gmail.com>
2024-02-08 17:42:44 +0530

    terraform

--- 03-vars/main.tf
+++ 03-vars/main.tf
@@ -21,7 +21,7 @@ output "fruit_list" {
   // value = element(var.fruits, 0)
 }
 
-#map
+#map plain
 variable "fruits_stock" {
   default = {
     apple=100
@@ -31,4 +31,22 @@ variable "fruits_stock" {
 
 output "fruits_stock_map" {
   value = var.fruits_stock["apple"]
+}
+
+#map of maps
+variable "fruit_stock_price" {
+  default = {
+    apple={
+      stock=100
+      price=1000
+    }
+    banana={
+      stock=50
+      price=100
+    }
+  }
+}
+
+output "fruits_mapsofmap_first" {
+  value = var.fruit_stock_price.apple["price"]
 }
