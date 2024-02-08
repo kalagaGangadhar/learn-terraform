2f25b834c4f148e9c6e5a767f68f6dd17fed0638
Gangadhar <gangadharkalaga@gmail.com>
2024-02-08 18:13:44 +0530

    terraform

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -6,3 +6,19 @@ output "fruit" {
   value = element(var.fruits,2)
 #  value = var.fruits[2] //getting error in this way
 }
+
+variable "fruit_with_stock" {
+  default = {
+    apple={
+      stock=100
+    }
+  }
+}
+
+output "fruit_stock" {
+  value = try(var.fruit_with_stock["banana"],0)
+}
+output "fruit_stock_price" {
+  value = lookup(var.fruit_with_stock["apple"], "price", 2)
+  // try(var.fruit_with_stock["apple"].price, 2)
+}
