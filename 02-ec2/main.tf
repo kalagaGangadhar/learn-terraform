6600db764b6dd6ac0b11107124af8201acaa08e8
Gangadhar <gangadharkalaga@gmail.com>
2024-02-08 16:48:00 +0530

    terraform

--- 02-ec2/main.tf
+++ 02-ec2/main.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "test" {
-  ami           = "ami-0f3c7d07486cad139"
+#  ami           = "ami-0f3c7d07486cad139"
+  ami = "ami-02d3fd86e6a2f5122"
   instance_type = "t2.micro"
   tags = {
     Name="Hello World"
