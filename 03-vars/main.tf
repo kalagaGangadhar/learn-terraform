05842b0c92eceb50d62a9f67dce29129518b0b1a
Gangadhar <gangadharkalaga@gmail.com>
2024-02-08 17:35:29 +0530

    terraform

--- 03-vars/main.tf
+++ 03-vars/main.tf
@@ -1,7 +1,34 @@
+#plain varible
 variable "test" {
   default="hello world"
 }
 
 output "op" {
   value = var.test
+}
+
+#list of variables
+variable "fruits" {
+  default = [
+    "apple",
+    "banana"
+  ]
+#  default = ["apple","orange"]  //single line syntax
+}
+
+output "fruit_list" {
+  value = var.fruits[0]
+  // value = element(var.fruits, 0)
+}
+
+#map
+variable "fruits_stock" {
+  default = {
+    name="apple"
+    stocke=100
+  }
+}
+
+output "fruits_stock_map" {
+  value = var.fruits_stock[0]
 }
