12827a48b11b2653fa9d2ac5493984a84397ed23
Gangadhar <gangadharkalaga@gmail.com>
2024-02-08 23:51:25 +0530

    terraform

---
+++ 06-loops/main.tf
@@ -0,0 +1,20 @@
+variable "component" {
+  default = ["frontend","mongodb","catalogue"]
+}
+
+resource "aws_instance" "instance" {
+  ami           = "ami-0f3c7d07486cad139"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-08db871bebc41e267" ]
+  count = length(var.component)
+  tags = {
+    Name=element(var.component,count.index )
+#    Name=var.component[count.index]
+  }
+}
+
+resource "aws_security_group" "all_tls" {
+  tags = {
+    Name=element(var.component,count.index )
+  }
+}
