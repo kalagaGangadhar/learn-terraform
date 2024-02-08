a6fbf0a72f2ee0d7ba958606cdd05baeb52ff360
Gangadhar <gangadharkalaga@gmail.com>
2024-02-09 00:00:41 +0530

    terraform

--- 05-data-sources/main.tf
+++ 05-data-sources/main.tf
@@ -1,8 +1,8 @@
-data "aws_ami_ids" "ami" {
+data "aws_ami" "ami" {
   owners = ["788002743912"]
   name_regex = "centos-8"
 }
 
 output "ami" {
-  value = data.aws_ami_ids.ami
+  value = data.aws_ami.ami
 }
