1d9167387a720fd69c17e579b400789f493a77c2
Gangadhar <gangadharkalaga@gmail.com>
2024-02-12 14:30:11 +0530

    terraform

--- 07-module/local-module/data.tf
+++ 07-module/local-module/data.tf
@@ -1,10 +1,8 @@
-provider "aws" {
-  region = "us-east-1"
-}
+
 data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
-  owners      = ["788002743912"]
+  owners      = ["973714476881"]
 }
 
 output "ami" {
