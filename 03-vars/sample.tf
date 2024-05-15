9ea60e12f53d234585ce054bf19473ab04e0a424
Gangadhar <gangadharkalaga@gmail.com>
2024-05-15 06:21:50 +0530

    learn terraform

---
+++ 03-vars/sample.tf
@@ -0,0 +1,34 @@
+variable "fruit" {
+  default = "apple"
+}
+
+variable "fruits_with_list" {
+  default = [
+  "apple",
+  "banana"
+  ]
+}
+
+variable "fruits_map" {
+  default = {
+    apple=10
+    banana=20
+  }
+}
+
+variable "fruits_map_maps" {
+  default = {
+    apple={
+      stock=10
+      price=100
+    }
+    banana={
+      stock=20
+      price=500
+    }
+  }
+}
+
+output "fruit_name" {
+  value = var.fruit
+}
