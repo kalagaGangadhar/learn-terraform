99bc5fd495f183cb72a6709e0857d0e31df4b142
Gangadhar <gangadharkalaga@gmail.com>
2024-11-26 23:00:49 +0530

    delete previous files

--- 02-ec2/server.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_instance" "example" {
-  ami           = "ami-0f3c7d07486cad139"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "Helloworld"
-  }
-}
--- 05-data-sources/main.tf
+++
@@ -1,8 +0,0 @@
-data "aws_ami_ids" "ami" {
-  name_regex = "Centos-*"
-  owners = ["788002743912"]
-}
-
-output "ami" {
-  value = data.aws_ami_ids.ami
-}
