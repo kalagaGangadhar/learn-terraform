40720432b9d200d0aab8b420c11dcdce75b8047e
Gangadhar <gangadharkalaga@gmail.com>
2024-12-15 19:01:01 +0530

    terraform

---
+++ 06-loops/for-each/main.tf
@@ -0,0 +1,16 @@
+variable "components" {
+  default = {
+    mongodb = { name = "mongodb-dev" }
+    catalogue = { name = "catalogue-dev" }
+  }
+}
+
+resource "aws_instance" "instance" {
+  for_each = var.components
+  ami = "ami-0b4f379183e5706b9"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-0f881d5350b65ec8c"]
+  tags = {
+    Name = lookup(var.components, each.value["name"], null)
+  }
+}
