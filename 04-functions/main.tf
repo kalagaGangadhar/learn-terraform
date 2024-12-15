5b95e45893530547229a551f9bdaf727d3c9cc0d
Gangadhar <gangadharkalaga@gmail.com>
2024-12-15 16:32:40 +0530

    terraform

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -17,5 +17,6 @@ variable "fruit_with_stock" {
 
 output "fruit_stock" {
   //value = var.fruit_with_stock["banana"].stock
-  value = try(var.fruit_with_stock["banana"].stock, 0)
+  value = try(var.fruit_with_stock["apple"].stock, 0) //try function: it checks for banana,if its not there, output is 0
 }
+
