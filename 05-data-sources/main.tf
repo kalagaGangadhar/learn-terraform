5d44f5d1f29796e398be11ee9b82b5e174cc84c8
Gangadhar <gangadharkalaga@gmail.com>
2024-12-15 16:57:38 +0530

    terraform

--- 05-data-sources/main.tf
+++ 05-data-sources/main.tf
@@ -1,7 +1,17 @@
+
 data "aws_instance" "instance" {
   instance_id = "i-0bc7a8432f3cc6d3d"
 }
 
 output "instance_details" {
   value = data.aws_instance.instance.private_ip
+}
+
+data "aws_ami_ids" "ami" {
+  owners = ["973714476881"]
+  name_regex = "centos-*"
+}
+
+output "ami" {
+  value = data.aws_ami_ids.ami
 }
