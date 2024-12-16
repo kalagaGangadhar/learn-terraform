cc0ebd5316659527f4995b721da5d4ea792e8ae6
Gangadhar <gangadharkalaga@gmail.com>
2024-12-16 11:21:18 +0530

    terraform

--- 06-loops/for-each/main.tf
+++ 06-loops/for-each/main.tf
@@ -1,7 +1,7 @@
 variable "components" {
   default = {
     redis = { name = "redis-dev" }
-    user = { name = "user-dev" }
+    user = { name = "cart-dev" }
     frontend = { name = "frontend-dev" }
     mongodb = { name = "mongodb-dev" }
     catalogue = { name = "catalogue-dev" }
