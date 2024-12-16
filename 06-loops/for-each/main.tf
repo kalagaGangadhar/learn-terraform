b7b95d08f100f41956fe9a49b5b0996def9f00c4
Gangadhar <gangadharkalaga@gmail.com>
2024-12-16 11:14:35 +0530

    terraform

--- 06-loops/for-each/main.tf
+++ 06-loops/for-each/main.tf
@@ -8,13 +8,13 @@ variable "components" {
 }
 
 resource "aws_instance" "instance" {
-  //for_each = var.components
+  for_each = var.components
   ami = "ami-0b4f379183e5706b9"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-0f881d5350b65ec8c"]
   tags = {
-    //Name = lookup(each.value, "name", null)
-    Name = lookup(var.components["frontend"],"name",null )
+    Name = lookup(each.value, "name", null)   # this can be used for multiple values
+    //Name = lookup(var.components["frontend"],"name",null ) # this can be used for single value
   }
 }
 resource "aws_security_group" "security" {
